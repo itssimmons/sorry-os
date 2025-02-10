ad36600582029a4d2ab0c23d037bbc8730de2771
Simón Villafañe <simonvillafanee@gmail.com>
2025-02-10 00:18:28 -0300

    hotfix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      exclude: path.resolve(__dirname, "src/app/icon.svg"),
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
